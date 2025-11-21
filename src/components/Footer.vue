4ee01d55ca6beaeda949ce49e4f935e716e8ae77
scuevas1 <scuevas1@hawk.iit.edu>
2025-11-20 20:58:36 -0600

    add: created the footer

---
+++ src/components/Footer.vue
@@ -0,0 +1,24 @@
+<template>
+  <footer class="footer">
+    <div class="footer-content">
+      <p class="footer-text">Made with love by Suheily Cuevas</p>
+
+      <a 
+        class="footer-email" 
+        href="mailto:scuevas1@hawk.illinoistech.edu"
+      >
+        Contact Me
+      </a>
+
+      <p class="footer-note">
+        © 2025 Suey’s Cookbook — All Recipes Made With Love
+      </p>
+    </div>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: "Footer"
+}
+</script>
