6397cd02a9d117367421f9b53e509f3b3150d1ea
scuevas1 <scuevas1@hawk.iit.edu>
2025-11-20 20:51:03 -0600

    add: created the navbar

---
+++ src/components/Navbar.vue
@@ -0,0 +1,19 @@
+<template>
+  <nav class="navbar">
+    <div class="nav-content">
+      <h1 class="nav-logo">Suey’s Cookbook</h1>
+
+      <ul class="nav-links">
+        <li><router-link to="/">Home</router-link></li>
+        <li><router-link to="/recipes">Recipes</router-link></li>
+        <li><router-link to="/about">About</router-link></li>
+      </ul>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: "Navbar"
+}
+</script>
